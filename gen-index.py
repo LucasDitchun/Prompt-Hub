906b270c139ee397e948efba2c81dba26e64ea0d
Lucas Ditchun <lucas.ditchun@gmail.com>
2024-11-22 17:46:51 -0300

    refactor: enhance markdown generation script with GitHub links
    Refactored `gen-index.py` to include:
    - Dynamic generation of clickable links for files in the markdown index.
    - Integration of a GitHub repository URL (`https://github.com/LucasDitchun/Prompt-Hub/blob/main`) to construct file paths.
    
    This improvement makes the generated `README-index.md` more interactive and user-friendly by linking directly to files in the GitHub repository.

--- gen-index.py
+++ gen-index.py
@@ -1,22 +1,23 @@
 import os
 
-def generate_markdown(directory):
+def generate_markdown(directory, github_repo_url):
     markdown_content = "# PROMPT HUB\n\n## Directory and Files Index\n\n"
+    
     for folder in os.listdir(directory):
         folder_path = os.path.join(directory, folder)
         
-        # Verificar se é uma pasta e não começa com '.' ou '_'
         if os.path.isdir(folder_path) and not folder.startswith(('.', '_')):
             markdown_content += f"### {folder}/\n"
             
-            # Listar arquivos dentro da pasta
             files = os.listdir(folder_path)
             if files:
                 markdown_content += f"#### Files in {folder}:\n"
                 for file in files:
                     file_path = os.path.join(folder_path, file)
+                    
                     if os.path.isfile(file_path) and not file.startswith(('.', '_')):
-                        markdown_content += f"- {file}\n"
+                        file_link = f"{github_repo_url}/{folder}/{file}"
+                        markdown_content += f"- [{file}]({file_link})\n"
             else:
                 markdown_content += "No files found in this folder.\n"
             
@@ -26,4 +27,6 @@ def generate_markdown(directory):
         md_file.write(markdown_content)
 
 current_directory = os.getcwd()
-generate_markdown(current_directory)
+github_repo_url = "https://github.com/LucasDitchun/Prompt-Hub/blob/main"
+
+generate_markdown(current_directory, github_repo_url)
